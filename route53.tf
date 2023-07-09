0734c1e315ec8a9988d75b3dc6c19d37d136fdb1
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-07-09 17:29:52 +0530

    test

--- route53.tf
+++ route53.tf
@@ -3,5 +3,5 @@ resource "aws_route53_record" "component" {
   name    = "${var.COMPONENT}-${var.ENV}"
   type    = "CNAME"
   ttl     = 300
-  records = [aws_lb.test.dns_name]
+  records = [aws_spot_instance_request.spot.private_ip]
 }
