3cd6c12da457623022115de2e68febb63cf72ee1
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-07-09 22:19:26 +0530

    test

--- main.tf
+++ main.tf
@@ -31,6 +31,9 @@ resource "aws_instance" "instance" {
 }
 
 resource "null_resource" "connect" {
+  triggers = {
+    ABC = timestamp()
+  }
   count = length(local.ALL_INSTANCE_IDS)
   provisioner "remote-exec" {
     connection {
