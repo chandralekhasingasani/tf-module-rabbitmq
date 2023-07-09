57950b80555e898e5d35ddc0094ab745f2cab76d
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-07-09 22:28:50 +0530

    test

--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "component" {
   zone_id = var.PRIVATE_HOSTED_ZONE_ID
   name    = "${var.COMPONENT}-${var.ENV}"
-  type    = "CNAME"
+  type    = "A"
   ttl     = 300
   records = local.ALL_INSTANCE_IPS
 }
